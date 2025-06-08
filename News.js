08cb707ee24078086084e3c30c5bbe42800e179a
jungwoo0719 <jungwoo07191@naver.com>
2025-06-09 01:06:55 +0900

    ISS 키워드만

--- News.js
+++ News.js
@@ -1,9 +1,13 @@
 const container = document.getElementById("news-container");
 
-fetch("https://api.spaceflightnewsapi.net/v4/articles/?limit=5")
+fetch("https://api.spaceflightnewsapi.net/v4/articles/?search=international%20space%20station&limit=5")
   .then(res => res.json())
   .then(data => {
     const articles = data.results;
+    if (articles.length === 0) {
+      container.innerHTML = "<p style='color:white;'>ISS 관련 기사가 없습니다.</p>";
+      return;
+    }
     articles.forEach(article => {
       const card = document.createElement("div");
       card.className = "news-card";
